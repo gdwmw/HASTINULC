e8ec935cf89c36a9ee7e3aea0932c52228676236
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2025-01-12 11:12:54 +0700

    refactor: 💡 navigation bar

---
+++ src/layouts/home/modules/nav/batches/content/index.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { Session } from "next-auth";
+import Image from "next/image";
+import Link from "next/link";
+import { FC, FormEvent, ReactElement, useEffect, useState } from "react";
+
+import logo from "@/public/assets/images/logos/Black.png";
+import { ExampleATWM } from "@/src/components/interfaces/example/A";
+import { NAVIGATION_DATA } from "@/src/libs/constants";
+
+interface I {
+  session: null | Session;
+}
+
+export const Content: FC<I> = (props): ReactElement => {
+  const [activeSection, setActiveSection] = useState("");
+
+  const handleSmoothScroll = (e: FormEvent, href: string) => {
+    e.preventDefault();
+    if (href === "#home") {
+      window.scrollTo({ behavior: "smooth", top: 0 });
+    } else {
+      document.getElementById(href.slice(1))?.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  useEffect(() => {
+    const handleActiveSection = () => {
+      const sections = NAVIGATION_DATA.map((item) => document.querySelector(item.href));
+
+      sections.forEach((section) => {
+        if (section) {
+          const rect = section.getBoundingClientRect();
+
+          if (rect.top <= window.innerHeight / 5 && rect.bottom >= window.innerHeight / 5) {
+            setActiveSection(section.id);
+          }
+        }
+      });
+    };
+
+    handleActiveSection();
+
+    window.addEventListener("scroll", handleActiveSection);
+
+    return () => window.removeEventListener("scroll", handleActiveSection);
+  }, []);
+
+  return (
+    <nav className="sticky inset-x-0 top-0 z-10 flex h-[88px] items-center justify-between bg-white px-10 text-lg">
+      <Image alt="Hastinulc Makeup Art" src={logo} width={210} />
+
+      <ul className="flex items-center gap-5 font-semibold">
+        {NAVIGATION_DATA.map((dt) => (
+          <li key={dt.id}>
+            <Link
+              className={`${activeSection === dt.href.substring(1) ? "border-b-2 border-rose-500 text-rose-500" : ""}`}
+              href={dt.href}
+              onClick={(e) => handleSmoothScroll(e, dt.href)}
+            >
+              {dt.label}
+            </Link>
+          </li>
+        ))}
+
+        <li className="active:scale-95">
+          {props.session?.user?.status ? (
+            <Image
+              alt="Photo Profile"
+              className="rounded-full border border-black p-0.5"
+              height={40}
+              src={props.session?.user?.image ?? ""}
+              width={40}
+            />
+          ) : (
+            <Link className={ExampleATWM({ color: "rose", size: "sm", variant: "solid" })} href={"/login"}>
+              LOGIN
+            </Link>
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
+};
