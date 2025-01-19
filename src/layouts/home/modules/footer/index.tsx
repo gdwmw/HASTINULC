730cecf5d18ec760c0f42820199444981ccd82c6
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2025-01-19 13:14:15 +0700

    feat: 🎉 footer

--- src/layouts/home/modules/footer/index.tsx
+++ src/layouts/home/modules/footer/index.tsx
@@ -1,3 +1,24 @@
+import Link from "next/link";
 import { FC, ReactElement } from "react";
 
-export const Footer: FC = (): ReactElement => <footer></footer>;
+export const Footer: FC = (): ReactElement => (
+  <footer className="mt-24 flex w-full justify-between bg-rose-300 px-10 py-5 text-xs text-white">
+    <section>
+      <p>&copy; 2025 Hastinulc Makeup Art, All rights reserved.</p>
+    </section>
+
+    <section>
+      <ul className="flex gap-5">
+        <li>
+          <Link href={""}>Terms of Service</Link>
+        </li>
+        <li>
+          <Link href={""}>Privacy Policy</Link>
+        </li>
+        <li>
+          <Link href={""}>Manage Cookies</Link>
+        </li>
+      </ul>
+    </section>
+  </footer>
+);
