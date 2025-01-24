4a2833025afd87d09bda999ea1ad3a13d29f8373
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2025-01-25 00:16:58 +0700

    refactor: 💡 context

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,18 +1,19 @@
 import { create } from "zustand";
 
+import { IBookingsPayloadContext } from "../types/context";
+
 interface IStates {
-  openA?: boolean;
+  booking?: IBookingsPayloadContext;
   openB?: boolean;
 }
 
 interface IActions {
-  setOpenA: (param: boolean) => void;
+  setBooking: (param: IBookingsPayloadContext) => void;
   setOpenB: (param: boolean) => void;
 }
 
 export const useGlobalStates = create<IActions & IStates>((set) => ({
-  openA: false,
   openB: false,
-  setOpenA: (openA: boolean) => set({ openA }),
+  setBooking: (booking: IBookingsPayloadContext) => set({ booking }),
   setOpenB: (openB: boolean) => set({ openB }),
 }));
---
+++ src/types/context/index.ts
@@ -0,0 +1,7 @@
+export interface IBookingsPayloadContext {
+  date?: string;
+  email?: string;
+  event?: string;
+  name?: string;
+  phoneNumber?: string;
+}
