19b14ab1f93a160b1af9aff63ef4beeec5b27a38
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2025-02-14 21:50:03 +0700

    refactor: 💡 html semantics

--- src/layouts/user/pages/history/index.tsx
+++ src/layouts/user/pages/history/index.tsx
@@ -1,11 +1,11 @@
 import { FC, ReactElement } from "react";
 
-import { Main } from "./modules";
+import { ASide } from "./modules";
 
 interface I {
   slug: Promise<{ slug: string[] }>;
 }
 
-const HistoryLayout: FC<I> = (props): ReactElement => <Main slug={props.slug} />;
+const HistoryLayout: FC<I> = (props): ReactElement => <ASide slug={props.slug} />;
 
 export default HistoryLayout;
--- src/layouts/user/pages/history/modules/aside/index.tsx
+++ src/layouts/user/pages/history/modules/aside/index.tsx
@@ -9,7 +9,7 @@ interface I {
   slug: Promise<{ slug: string[] }>;
 }
 
-export const Main: FC<I> = async (props): Promise<ReactElement> => {
+export const ASide: FC<I> = async (props): Promise<ReactElement> => {
   const slug = (await props.slug).slug;
   const session = await getAllSession();
   const response = await GETBookings(`sort[0]=current:desc&filters[data][documentId][$eq]=${session?.user?.datasDocumentId}`);
