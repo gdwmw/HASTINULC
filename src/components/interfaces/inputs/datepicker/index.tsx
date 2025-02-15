760ecc38fccbbc5fc5d7b9f7998b4ad405091443
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2025-02-15 14:24:09 +0700

    feat: 🎉 component date picker input

---
+++ src/components/interfaces/inputs/datepicker/index.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { FC, ReactElement } from "react";
+import DatePicker, { DatePickerProps } from "react-datepicker";
+
+import { twm } from "@/src/libs/tailwind-merge";
+
+import { ErrorMessage, InputsContainer, Label } from "../elements";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+/* eslint-disable perfectionist/sort-union-types */
+type TDatePicker = {
+  className?: string;
+  color?: "rose" | "emerald";
+  containerClassName?: string;
+  disabled?: boolean;
+  errorMessage?: string;
+  fieldsetClassName?: string;
+  label?: string;
+  legendClassName?: string;
+} & DatePickerProps;
+/* eslint-enable perfectionist/sort-union-types */
+
+const DatePickerTWM = ({ className, disabled }: TDatePicker) =>
+  twm(
+    "w-full rounded-lg bg-transparent px-2 outline-none disabled:cursor-not-allowed",
+    disabled ? "text-gray-400" : "text-gray-800 hover:bg-gray-50 focus:bg-gray-50",
+    className,
+  );
+
+export const DatePickerInput: FC<TDatePicker> = ({
+  className,
+  color,
+  containerClassName,
+  disabled,
+  errorMessage,
+  fieldsetClassName,
+  label,
+  legendClassName,
+  ...props
+}): ReactElement => (
+  <InputsContainer className={containerClassName}>
+    <Label
+      color={color}
+      disabled={disabled}
+      errorMessage={errorMessage}
+      fieldsetClassName={fieldsetClassName}
+      label={label}
+      legendClassName={legendClassName}
+    >
+      <DatePicker className={DatePickerTWM({ className, disabled })} data-testid="datepicker" disabled={disabled} {...props} />
+    </Label>
+
+    {errorMessage && !disabled && <ErrorMessage errorMessage={errorMessage} />}
+  </InputsContainer>
+);
+
+DatePickerInput.displayName = "DatePickerInput";
--- src/components/interfaces/inputs/index.ts
+++ src/components/interfaces/inputs/index.ts
@@ -1,3 +1,4 @@
+export * from "./datepicker";
 export * from "./input";
 export * from "./select";
 export * from "./text-area";
