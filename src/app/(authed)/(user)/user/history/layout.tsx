c7f9164f31bf8f9c19c92f1a65dcb91cea15a8ca
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2025-02-18 18:20:53 +0700

    refactor: 💡 history

--- src/app/(authed)/(user)/user/history/layout.tsx
+++ src/app/(authed)/(user)/user/history/layout.tsx
@@ -1,21 +1,9 @@
 import { FC, PropsWithChildren, ReactElement } from "react";
 
-import { getAllSession } from "@/src/hooks/session";
 import { GlobalHistoryLayout } from "@/src/layouts/user/pages/history/global";
-import { GETBookings, GETReviews } from "@/src/utils/api";
 
 type T = Readonly<PropsWithChildren>;
 
-const Layout: FC<T> = async (props): Promise<ReactElement> => {
-  const session = await getAllSession();
-  const bookingsResponse = await GETBookings(`sort[0]=current:desc&filters[data][documentId][$eq]=${session?.user?.datasDocumentId}`);
-  const reviewsResponse = await GETReviews(`filters[username][$eq]=${session?.user?.username}`);
-
-  return (
-    <GlobalHistoryLayout bookingsResponse={bookingsResponse} reviewsResponse={reviewsResponse} session={session}>
-      {props.children}
-    </GlobalHistoryLayout>
-  );
-};
+const Layout: FC<T> = (props): ReactElement => <GlobalHistoryLayout>{props.children}</GlobalHistoryLayout>;
 
 export default Layout;
---
+++ src/layouts/user/pages/history/global/modules/main/index.tsx
@@ -0,0 +1,20 @@
+import { FC, PropsWithChildren, ReactElement } from "react";
+
+import { getAllSession } from "@/src/hooks/session";
+import { GETBookings, GETReviews } from "@/src/utils/api";
+
+import { Content } from "./batches";
+
+type T = Readonly<PropsWithChildren>;
+
+export const Main: FC<T> = async (props): Promise<ReactElement> => {
+  const session = await getAllSession();
+  const bookingsResponse = await GETBookings(`sort[0]=current:desc&filters[data][documentId][$eq]=${session?.user?.datasDocumentId}`);
+  const reviewsResponse = await GETReviews(`filters[username][$eq]=${session?.user?.username}`);
+
+  return (
+    <Content bookingsResponse={bookingsResponse} reviewsResponse={reviewsResponse} session={session}>
+      {props.children}
+    </Content>
+  );
+};
