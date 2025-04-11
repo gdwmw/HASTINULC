4f197535121e05f0caa37c3ca8b46184fb947abc
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2025-04-11 10:06:46 +0700

    refactor: 💡 types

---
+++ src/types/components/index.ts
@@ -0,0 +1,15 @@
+import { EXAMPLEA_COLOR_OPTIONS, EXAMPLEA_SIZE_OPTIONS, EXAMPLEA_VARIANT_OPTIONS, EXAMPLEC_COLOR_OPTIONS, LABEL_COLOR_OPTIONS } from "@/src/libs";
+
+// ----------------------------
+
+export type TExampleAVariant = (typeof EXAMPLEA_VARIANT_OPTIONS)[number];
+export type TExampleAColor = (typeof EXAMPLEA_COLOR_OPTIONS)[number];
+export type TExampleASize = (typeof EXAMPLEA_SIZE_OPTIONS)[number];
+
+// ----------------------------
+
+export type TExampleCColor = (typeof EXAMPLEC_COLOR_OPTIONS)[number];
+
+// ----------------------------
+
+export type TLabelColor = (typeof LABEL_COLOR_OPTIONS)[number];
