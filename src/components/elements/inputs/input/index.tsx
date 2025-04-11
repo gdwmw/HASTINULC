f7558ac8d9c809cd9ca3ba19a0069860bff00ba6
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2025-04-11 11:05:23 +0700

    refactor: 💡 components

---
+++ src/components/elements/inputs/input/index.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { DetailedHTMLProps, FC, forwardRef, InputHTMLAttributes, ReactElement, ReactNode } from "react";
+
+import { twm } from "@/src/libs";
+
+import { ErrorMessage, ILabel, InputsContainer, Label } from "../";
+import { ExampleA } from "../..";
+
+interface I
+  extends DetailedHTMLProps<Omit<InputHTMLAttributes<HTMLInputElement>, "className" | "color">, HTMLInputElement>,
+    Omit<ILabel, "children" | "className"> {
+  className?: {
+    container?: string;
+    input?: string;
+  } & ILabel["className"];
+  icon?: ReactNode;
+  iconOnClick?: () => void;
+}
+
+const InputTWM = ({ className, disabled }: Omit<I, "color" | "label">) =>
+  twm("w-full rounded-sm bg-transparent px-1 outline-none disabled:cursor-not-allowed", disabled && "text-gray-400", className?.input);
+
+export const Input: FC<I> = forwardRef<HTMLInputElement, I>(
+  ({ className, color, disabled, errorMessage, icon, iconOnClick, label, ...props }, ref): ReactElement => (
+    <InputsContainer className={className?.container}>
+      <Label
+        className={{ fieldset: className?.fieldset, legend: className?.legend }}
+        color={color}
+        disabled={disabled}
+        errorMessage={errorMessage}
+        label={label}
+      >
+        <input className={InputTWM({ className, disabled })} data-testid="input" disabled={disabled} ref={ref} {...props} />
+
+        {icon && (
+          <ExampleA
+            className={`ml-2 mr-1 ${!disabled && "text-inherit"} ${errorMessage ? "hover:text-red-600 active:text-red-700" : ""}`}
+            color={color}
+            disabled={disabled}
+            onClick={iconOnClick}
+            size="sm"
+            type="button"
+            variant="ghost"
+          >
+            {icon}
+          </ExampleA>
+        )}
+      </Label>
+
+      {errorMessage && !disabled && <ErrorMessage errorMessage={errorMessage} />}
+    </InputsContainer>
+  ),
+);
+
+Input.displayName = "Input";
---
+++ src/components/elements/inputs/select/index.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { DetailedHTMLProps, FC, forwardRef, ReactElement, SelectHTMLAttributes } from "react";
+
+import { twm } from "@/src/libs";
+
+import { ErrorMessage, ILabel, InputsContainer, Label } from "../";
+
+interface I
+  extends DetailedHTMLProps<Omit<SelectHTMLAttributes<HTMLSelectElement>, "className" | "color">, HTMLSelectElement>,
+    Omit<ILabel, "children" | "className"> {
+  className?: {
+    container?: string;
+    select?: string;
+  } & ILabel["className"];
+}
+
+const SelectTWM = ({ className, disabled }: Omit<I, "color" | "label">) =>
+  twm("w-full rounded-sm bg-transparent px-1 outline-none disabled:cursor-not-allowed", disabled && "text-gray-400", className?.select);
+
+export const Select: FC<I> = forwardRef<HTMLSelectElement, I>(
+  ({ className, color, disabled, errorMessage, label, ...props }, ref): ReactElement => (
+    <InputsContainer className={className?.container}>
+      <Label
+        className={{ fieldset: className?.fieldset, legend: className?.legend }}
+        color={color}
+        disabled={disabled}
+        errorMessage={errorMessage}
+        label={label}
+      >
+        <select className={SelectTWM({ className, disabled })} data-testid="select" disabled={disabled} ref={ref} {...props}>
+          {props.children}
+        </select>
+      </Label>
+
+      {errorMessage && !disabled && <ErrorMessage errorMessage={errorMessage} />}
+    </InputsContainer>
+  ),
+);
+
+Select.displayName = "Select";
