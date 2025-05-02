a7ac68c07d1b1db35f19202979dd25fb5558463a
Gede Dewo Wahyu M.W <gededewowahyumw@gmail.com>
2025-05-02 15:51:10 +0700

    refactor: 💡 error message

--- src/hooks/functions/index.ts
+++ src/hooks/functions/index.ts
@@ -5,6 +5,22 @@ import { IDataResponse } from "@/src/types";
 
 // ----------------------------
 
+export const getErrorMessage = (error: unknown): string | undefined => {
+  const errorMessage =
+    error instanceof Error
+      ? error.message
+          .split("Message: ")
+          .pop()
+          ?.split(" ")
+          .map((word: string) => word.charAt(0).toUpperCase() + word.slice(1))
+          .join(" ")
+      : "An Unknown Error Occurred";
+
+  return errorMessage;
+};
+
+// ----------------------------
+
 const CURRENCY_SETTINGS = {
   EUR: {
     fractionDigits: 2,
